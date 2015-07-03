a5797d499dd4697290f730b1ac39f73631a95fd7
hienbx94 <hienbx94@gmail.com>
2015-07-03 13:09:28 +0700

    Categories-show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,12 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.paginate page: params[:page]
+  end
+
+  def show
+    @category = Category.find params[:id]
+  end
+
   def new
     @category = Category.new
   end
