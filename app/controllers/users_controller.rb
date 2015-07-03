000bb86fc6bbca8e60ec5de86c21f1fafc340749
Tran Thanh Tung <tran.thanh.tung@framgia.com>
2015-07-03 16:31:00 +0700

    Log in log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = I18n.t "signup_success"
       redirect_to @user
     else
