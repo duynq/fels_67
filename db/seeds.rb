aa0076c21b0ee71321c7c37031d7f74e745d4445
Tran Thanh Tung <tran.thanh.tung@framgia.com>
2015-07-06 17:02:18 +0700

    Finish show users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(username:  "Tran Thanh Tung",
+             email: "trantube94@gmail.com",
+             password:              "19101994",
+             password_confirmation: "19101994",
+             admin: true)
+50.times do |n|
+  name  = Faker::Name.name
+  email = "test.aaaaa-#{n+1}@gmail.com"
+  password = "abc123"
+  User.create!(username: name,
+              email: email,
+              password:              password,
+              password_confirmation: password)
+end
