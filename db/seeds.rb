fb8435d17e5d4e762c9924399eb278e2275b0c6b
Tran Thanh Tung <tran.thanh.tung@framgia.com>
2015-07-09 15:32:25 +0700

    Finish edit-user-show

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,15 @@ User.create!(name:  "Tran Thanh Tung",
               password:              password,
               password_confirmation: password)
 end
+
+5.times do |n|
+  name  = Faker::Name.name
+  image = Faker::Name.name
+  Category.create!(name: name, image: image)
+end
+
+users = User.order(:created_at).take(6)
+10.times do
+  category_id = rand(1..5)
+  users.each { |user| user.lessons.create!(category_id: category_id) }
+end
